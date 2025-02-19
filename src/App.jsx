ebe0e0d61985a90c32ddc1a5a39ffa065bb7c6f4
doyi0107 <doyi9917@naver.com>
2025-02-20 00:20:27 +0900

    초기셋팅-eslint,prettier설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <h1 class="text-3xl font-bold underline">Vite + React</h1>
+        <h1 className="text-3xl font-bold underline">Vite + React</h1>
       </div>
     </>
   );
 }
 
-export default App
+export default App;
