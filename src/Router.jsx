bd4480aa2c1639425eb8011941c9db357c8d0c6f
doyi0107 <doyi9917@naver.com>
2025-02-20 15:12:14 +0900

    router 설정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import BookmarkList from "../pages/bookmarkList/bookmarkList";
 import App from "../App";
-import WorkDetail from "../pages/Work/WorkDetail";
-import DelayRouteWrapper from "../pages/Work/components/error/DelayRouteWrapper";
 
 const router = createBrowserRouter([
   {
