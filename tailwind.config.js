79ddd2c5ab5d0928cce49d65f554fc4c1762f320
urjimyu <92876819+urjimyu@users.noreply.github.com>
2025-02-20 14:12:38 +0900

    init: theme

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E3A8A",
+        secondary: "#FBBF24",
+        danger: "#DC2626",
+      },
+      fontSize: {
+        sm: "14px",
+        base: "16px",
+        lg: "20px",
+        xl: "24px",
+      },
+    },
+  },
+};
