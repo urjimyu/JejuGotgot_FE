6963c323825f5c7cb4f8c8abfe3e3a57e0e48434
doyi0107 <doyi9917@naver.com>
2025-02-21 02:00:13 +0900

    [feat/후기 작성 이미지 연결]

--- src/pages/reviewCreate/reviewCreate.jsx
+++ src/pages/reviewCreate/reviewCreate.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import { useLocation } from 'react-router-dom'; // 🔹 useLocation 추가
 import './reviewCreate.css';
 import Card from '../../components/card/Card';
+import { getFormattedDate } from '../../utils/dateUtils';
 
 export default function ReviewCreate() {
   const location = useLocation();
   const placeData = location.state?.location || {}; // 🔹 받은 데이터가 없을 경우 빈 객체 처리
 
   return (
-    <div>
+    <div className='review-wrap'>
       <div>
-        <p>현재 년월일</p>
-        <div>
-          <div>{placeData.title || '가게 이름 없음'}</div>
-          <div>{placeData.type || '가게 태그 없음'}</div>
+        <p className='review-date'>{getFormattedDate()}</p>
+        <div className='review-info'>
+          <div className='review-store-title'>{placeData.title || '가게 이름 없음'}</div>
         </div>
-        <div>{placeData.subtitle || '가게 주소 없음'}</div>
+        <div className='review-store-address'>{placeData.subtitle || '가게 주소 없음'}</div>
       </div>
       <Card disableBookmark={true} location={placeData} />
     </div>
