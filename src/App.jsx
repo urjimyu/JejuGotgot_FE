ba158ebc3b219a4f233b21c013609ffd75ea517b
urjimyu <92876819+urjimyu@users.noreply.github.com>
2025-02-20 13:13:07 +0900

    init: gds 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
+import { ThemeProvider } from '@goorm-dev/vapor-core';
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <ThemeProvider>
       <div>
         <h1 className="text-3xl font-bold underline">Vite + React</h1>
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
