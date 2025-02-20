fb0c7658dfabee8b9935b10d69e3f06a6a049fe7
urjimyu <92876819+urjimyu@users.noreply.github.com>
2025-02-20 15:52:48 +0900

    init: vapor

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from '@goorm-dev/vapor-core';
+
 import './index.css'
 import App from './App.jsx'
 
@@ -7,6 +9,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThemeProvider>
     <App />
+    </ThemeProvider>
   </StrictMode>,
 )
